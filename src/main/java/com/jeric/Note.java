0896ee7cd53a5df61a11ef092cb9c0a87a3b90bb
Jerickson05 <jerickson05.github@gmail.com>
2025-04-16 11:42:46 -0500

    Roughed out Note Class

---
+++ src/main/java/com/jeric/Note.java
@@ -0,0 +1,8 @@
+public class Note
+{
+//Might need to abstract this.
+//The Concretes would be: Flat, Sharp, Natural (and maybe DblFlat and DblSharp).
+//TODO: Figure this out.
+public Note() {}
+
+}
