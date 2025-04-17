5bdfff082dd92a65ee42d606b92d990b8ccb4923
Jerickson05 <jerickson05.github@gmail.com>
2025-04-17 12:19:06 -0500

    Removed the FXML parts of the JavaFX Program and began tinkering with some other files.

--- src/main/java/com/jeric/Note.java
+++ src/main/java/com/jeric/Note.java
@@ -3,6 +3,6 @@ public class Note
 //Might need to abstract this.
 //The Concretes would be: Flat, Sharp, Natural (and maybe DblFlat and DblSharp).
 //TODO: Figure this out.
-public Note() {}
+public Note(){}
 
 }
